05bdb58ecd4a9be3dceb69fdae559276c3d52fb6
Berke Algül <47500887+berkealgul@users.noreply.github.com>
2020-09-29 14:23:08 +0300

    Add files via upload

--- aruco_slam.cpp
+++ aruco_slam.cpp
@@ -1,54 +1,58 @@
-#include <iostream>
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/image_encodings.h>
 #include "ar_tracker/tracker.h"
+
+bool new_frame = false;
+cv::Mat frame;
+
+void camera_cb(const sensor_msgs::ImageConstPtr& msg)
+{
+	cv_bridge::CvImagePtr cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+    frame = cv_ptr->image;
+    new_frame = true;
+}
 
 int main(int argc, char **argv)
 {
 	ros::init(argc, argv, "aruco_slam");
 	ros::NodeHandle n;
 
-	VideoCapture cap;
-	cap.open(1);
+	std::string camera_topic;
+	n.getParam("/camera/color_topic", camera_topic);
 
-	if(!cap.isOpened())
-	{
-		std::cout << "-------------------------------------------\n\n";
-		std::cout << "   Camera is not open! Shutting down!\n\n";
-		std::cout << "-------------------------------------------";
-		return 1;
-	}
-	else
-		std::cout << "---------------Camera Opended!!!--------------\n";
+	image_transport::ImageTransport it(n);
+	image_transport::Subscriber camera_sub = it.subscribe(camera_topic, 1, camera_cb);
 
-
-	// we need better calibration and c922 has auto focus
 	Mat_<double> mtx(3,3);
 	Mat_<double> dist(1,5);
-	dist << 0,0,0,0,0;
-	mtx << 395, 0.0, 320,
-		   0.0, 395, 240,
+	dist << 0.020436355102596344, -0.11407839179793304, 0.004229887050454093, -0.01709654130034178, 0.13991605472148272;
+	mtx << 627.2839475395182, 0.0, 295.0153571445745,
+		   0.0, 630.6046803340988, 237.10098847214766,
 		   0.0, 0.0, 1.0;
 
 	ArTracker::parameters *params = ArTracker::create_parameters();
 
-	//params->dist = dist;
-	//params->mtx = mtx;
+	params->dist = dist;
+	params->mtx = mtx;
 
-	/*
 	ArTracker::Tracker tracker(params);
 
 	while(ros::ok())
 	{
-		Mat frame;
-		cap.read(frame);
-		tracker.run_frame(frame);
-		tracker.broadcast_tree();
 
-		int x = waitKey(1);
-		imshow("frame", frame);
+		if(new_frame)
+		{
+			tracker.run_frame(frame);
+			tracker.broadcast_tree();
+			int x = waitKey(1);
+			imshow("frame", frame);
+			new_frame = false;
+		}
+		
+		ros::spinOnce();
+		
 	}
-	*/
-
-	ArTracker::run(cap, params);
 
 	return 0;
 }
